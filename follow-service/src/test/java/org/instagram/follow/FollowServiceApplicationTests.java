a5c9b49b10812b7917e6603d1bf8a0d64b532a3e
sasasavic03 <sasasavic2003@gmail.com>
2026-02-23 13:06:32 +0100

    add initial structure for auth and follow services

---
+++ follow-service/src/test/java/org/instagram/follow/FollowServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.instagram.follow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
